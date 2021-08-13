adf34bab826cd3d01316590eb2bebb9f9c71e702
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-08-13 12:08:35 +0200

    Prepare the client and server. Add the function of storing the list od users and information about the login of a nwe user.

--- client/app.js
+++ client/app.js
@@ -7,33 +7,49 @@ const messageContentInput = document.getElementById('message-content');
 
 let userName = '';
 
+const socket = io();
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('newUser', ({ author, content }) => addMessage(author, content));
+socket.on('userLeft', ({ author, content }) => addMessage(author, content));
+
 const login = (e) => {
   e.preventDefault();
-  if (!userNameInput.value) {
+  if (userNameInput.value) {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  } else {
     alert('Please type your name');
-  };
-  userName = userNameInput.value;
-  loginForm.classList.toggle('show');
-  messagesSection.classList.toggle('show');
+  }
+  socket.emit('login', userName);
 }
 
 const sendMessage = (e) => {
   e.preventDefault();
   if (messageContentInput.value) {
     addMessage(userName, messageContentInput.value);
+    socket.emit('message', {
+      author: userName,
+      content: messageContentInput.value,
+    });
     messageContentInput.value = '';  //Czyścimy pole tekstowe
   } else {
     alert('Please write your message');
   }
-};
+}
 
 const addMessage = (author, content) => {
   const message = document.createElement('li');
   message.classList.add('message');
   message.classList.add('message--received');
-  if (author === userName) message.classList.add('message--self');
+  if (author === userName) {
+    message.classList.add('message--self');
+  }
+  if (author === 'ChatBot') {
+    message.classList.add('message--chatBot');
+  }
   message.innerHTML = `
-    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+    <h3 class="message__author">${userName === author ? "You" : author}</h3>
     <div class="message__content">
       ${content}
     </div>
--- server.js
+++ server.js
@@ -1,20 +1,60 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
+
+const messages = [];
+const users = [];
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/client')));
 
-const message = [];
-
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
+const server = app.listen(8000, () => {
+  console.log('Server is running on Port:', 8000);
 });
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+const io = socket(server);
+
+io.on('connection', (socket) => {  // Nasłuchiwacz na automatyczny event ('connection') emitowany przez klienta socket, który się podłącza
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('login', (login) => {
+    console.log('new user logged' + socket.id);
+    users.push({ login, id: socket.id });
+    socket.broadcast.emit('newUser', {
+      author: 'Chat Bot',
+      content: `${login} has joined the conversation!`,
+    });
+  });
+
+  socket.on('message', (message) => {
+    console.log("Oh, I've got something from " + socket.id);
+    messages.push(message);  // Dzięki broadcast emitujemu event do wszystkich socketów, oprócz tego, z którego go wysyłamy
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {  // Nasłuchiwacz na zamknięcie połączenia z klientem, który w momencie zamknięcia również emituje automatyczny event 'disconnect'
+    console.log('Oh, socket ' + socket.id + ' has left');
+
+    const leavingUser = users.find((user) => user.id == socket.id);
+    const userIndex = users.indexOf(leavingUser);
+    if (leavingUser) {
+      socket.broadcast.emit('userLeft', {
+        author: 'Chat Bot',
+        content: `${leavingUser ? leavingUser.login : 'user'
+          } has left the conversation!`,
+      });
+      users.splice(userIndex, 1);
+    }
+  });
+
+  console.log("I've added a listener on message event \n");
 });
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+});
+
